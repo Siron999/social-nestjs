ab2a78f29d20038bd2448f1af391bfb06d687919
Siron <sironshakya10@gmail.com>
2021-10-06 22:53:09 +0545

    env removed

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(0);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
