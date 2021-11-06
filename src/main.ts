13c7943f40ff76595afb0aa174883870f394e2d3
DESKTOP-66RMSKE\Siron <siron.shakya@greenqbit.com>
2021-11-06 19:32:16 +0545

    payload limit set

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 const  dotenv= require('dotenv').config({path:"./.env"});
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
